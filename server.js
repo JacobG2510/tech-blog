238ff6e3c900d601b594edac9ce33f20e879511e
blastking2510@gmail.com <blastking2510@gmail.com>
2023-09-28 23:17:18 -0700

    please work

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const path = require('path');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const hbs = exphbs.create({ helpers });
+
+// Session middleware
+const sess = {
+  secret: 'yourSecretKeyHere',
+  cookie: { maxAge: 86400000 }, // 24 hours
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+});
